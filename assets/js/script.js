7f5eee35a3b97d9f77006e8ed8644a50569dc8fe
Miller Kisley <milkisly@gmail.com>
2021-07-16 11:59:25 -0300

    class in object data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        isActive: true,
-        hasError: false
+        classObject: { 
+            active: true, 
+            'text-danger': false  
+        }
     }
 })
