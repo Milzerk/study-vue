d3dd9633a5f5a41796254af999affa7a619ea923
Miller Kisley <milkisly@gmail.com>
2021-07-16 11:41:21 -0300

    computed vs watch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,54 @@
-var app = new Vue({
+var vm = new Vue({
     el: '#app',
     data: {
-        rawHtml: '<span style="color:red"> Sou Vermelho </span>',
-        dynamicId: 12,
-        isButtonDisabled: true,
-        seen: true,
-        attributename: 'href',
-        url: 'https://www.google.com'
+        message: 'Olá Vue'
+    },
+    computed: {
+        // uma função "getter" computada (computed getter)
+        reversedMessage: function () {
+            // `this` aponta para a instância Vue da variável `vm`
+            return this.message.split('').reverse().join('')
+        },
+        now: function () {
+            return Date.now()
+        }
+    },
+    methods: {
+        reverseMessage: function () {
+            return this.message.split('').reverse().join('')
+        },
+        nowTime: function () {
+            return Date.now()
+        }
     }
-});
+})
+
+// var vm = new Vue({
+//     el: '#demo',
+//     data: {
+//         firstName: 'Foo',
+//         lastName: 'Bar',
+//         fullName: 'Foo Bar'
+//     },
+//     watch: {
+//         firstName: function (val) {
+//             this.fullName = val + ' ' + this.lastName
+//         },
+//         lastName: function (val) {
+//             this.fullName = this.firstName + ' ' + val
+//         }
+//     }
+// })
+
+var vm = new Vue({
+    el: '#demo',
+    data: {
+        firstName: 'Foo',
+        lastName: 'Bar'
+    },
+    computed: {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
+        }
+    }
+})
