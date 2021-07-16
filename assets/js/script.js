be1f56fbda12bba929185801bdc077b5f931e1d5
Miller Kisley <milkisly@gmail.com>
2021-07-16 17:37:40 -0300

    passando objeto para component

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Definindo novo componente chamado button-counter
 Vue.component('button-counter', {
-  data: function() {
+  data: function () {
     return {
       count: 0
     }
@@ -8,6 +8,23 @@ Vue.component('button-counter', {
   template: '<button v-on:click="count++">Você clicou em mim {{ count }} vezes.</button>'
 })
 
-new Vue({ 
-  el: '#components-demo'
- })
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+new Vue({
+  el: '#components-demo',
+  data: {
+    posts: [
+      { id: 1, title: '- Minha jornada com Vue', content: '<h4>conteúdo 1</h4>' },
+      { id: 2, title: '- Postagens com Vue', content: '<h4>conteúdo 2</h4>' },
+      { id: 6, title: '- Porque Vue é tão divertido', content: '<h4>conteúdo 3</h4>' }
+    ]
+  }
+})
