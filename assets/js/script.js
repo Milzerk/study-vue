84a67e6641c33c3762b4de25a3e8ba5d34d0de5b
Miller Kisley <milkisly@gmail.com>
2021-07-16 10:54:51 -0300

    texto puro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-var dados = {a: 1}
-
-var zeni = new Vue({
+var app = new Vue({
     el: '#app',
-    data: dados,
-    created: function () {
-        // `this` aponta para a instância
-        console.log('a é: ' + this.a)
+    data: {
+        msg: 'Hello Humans!'
     }
-})
-
-console.log(document.getElementById('app'));
-  // => "a é: 1"
+});
