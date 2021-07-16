77d8ccd076752fd574f79112479058c58d79dfb3
Miller Kisley <milkisly@gmail.com>
2021-07-16 15:37:48 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,6 @@
-Vue.component('todo-item', {
-    template: '\
-      <li>\
-        {{ title }}\
-        <button v-on:click="$emit(\'remove\')">Remove</button>\
-      </li>\
-    ',
-    props: ['title']
-  })
-  
-  new Vue({
-    el: '#todo-list-example',
+var example1 = new Vue({
+    el: '#example-1',
     data: {
-      newTodoText: '',
-      todos: [
-        {
-          id: 1,
-          title: 'Lavar os pratos',
-        },
-        {
-          id: 2,
-          title: 'Tirar o lixo',
-        },
-        {
-          id: 3,
-          title: 'Cortar a grama'
-        }
-      ],
-      nextTodoId: 4
-    },
-    methods: {
-      addNewTodo: function () {
-        this.todos.push({
-          id: this.nextTodoId++,
-          title: this.newTodoText
-        })
-        this.newTodoText = ''
-      }
+        counter: 0
     }
-  })
+})
