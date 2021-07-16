b57faa44363af64d8e94b9266841a5d720eabce1
Miller Kisley <milkisly@gmail.com>
2021-07-16 15:35:07 -0300

    🎓 Final renderização com for

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
-var vm = new Vue({
-    el: '#app',
+Vue.component('todo-item', {
+    template: '\
+      <li>\
+        {{ title }}\
+        <button v-on:click="$emit(\'remove\')">Remove</button>\
+      </li>\
+    ',
+    props: ['title']
+  })
+  
+  new Vue({
+    el: '#todo-list-example',
     data: {
-        items: [
-            { message: 'Algo' },
-            { message: 'Outro' }
-        ]
+      newTodoText: '',
+      todos: [
+        {
+          id: 1,
+          title: 'Lavar os pratos',
+        },
+        {
+          id: 2,
+          title: 'Tirar o lixo',
+        },
+        {
+          id: 3,
+          title: 'Cortar a grama'
+        }
+      ],
+      nextTodoId: 4
+    },
+    methods: {
+      addNewTodo: function () {
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText
+        })
+        this.newTodoText = ''
+      }
     }
-})
+  })
