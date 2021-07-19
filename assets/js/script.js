d3e526fa53f0a5116edef9a6e7ef3367903e885c
Miller Kisley <milkisly@gmail.com>
2021-07-19 11:27:06 -0300

    validações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
+Vue.component('my-component', {
+  props: {
+    // Checagem básica de tipos (`null` and `undefined` passarão em qualquer validação de tipo)
+    propA: Number,
+    // Multíplos tipos possíveis
+    propB: [String, Number],
+    // String obrigatória
+    propC: {
+      type: String,
+      required: true
+    },
+    // Número com um valor padrão
+    propD: {
+      type: Number,
+      default: 100
+    },
+    // Objeto com um valor padrão
+    propE: {
+      type: Object,
+      // Objetos ou Arrays padrões devem ser retornadas
+      // a partir de uma função fabricadora
+      default: function () {
+        return { message: 'hello' }
+      }
+    },
+    // Função personalizada de validação
+    propF: {
+      validator: function (value) {
+        // O valor precisa corresponder a alguma dessas Strings
+        return ['success', 'warning', 'danger'].indexOf(value) !== -1
+      }
+    }
+  },
+  template: '<div><p>propA: {{propA}}</p><p>propB: {{propB}}</p><p>propC: {{propC}}</p><p>propD: {{propD}}</p><p>propE: {{propE.nome}}</p><p>propF: {{propF}}</p></div>'
+})
+
 Vue.component('blog-post', {
   // camelCase em JavaScript
   props: ['postTitle'],
@@ -14,6 +50,12 @@ var vm = new Vue({
   data: {
     theValue:  {
       valor: 'Valor'
-    }
+    },
+    dataA: 59,
+    dataB: '50',
+    dataC: 'O valor C',
+    dataD: 800,
+    dataE: {nome: 'miller'},
+    dataF: 'success'
   }
 });
