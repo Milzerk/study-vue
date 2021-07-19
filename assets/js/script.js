c4e59d95611c5d33851911e8085b60cba30442f2
Miller Kisley <milkisly@gmail.com>
2021-07-19 11:42:46 -0300

    model checkbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+Vue.component('base-checkbox', {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean
+  },
+  template: `
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('change', $event.target.checked)"
+    >
+  `
+});
+
 var vm = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    lovingVue: false
+  }
 });
