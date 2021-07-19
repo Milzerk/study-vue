759c095fb35dbce37d323b8527378880a6e0731c
Miller Kisley <milkisly@gmail.com>
2021-07-19 10:29:38 -0300

    ✨ page with vue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
-Vue.component("tab-home", {
-  template: "<div>Home component</div>"
-});
-Vue.component("tab-posts", {
-  template: "<div>Posts component</div>"
-});
-Vue.component("tab-archive", {
-  template: "<div>Archive component</div>"
-});
+Vue.component('home-box', {
+  props: ['post'],
+  template: '<div><img src="./assets/img/images.png"><p>{{post.title}}</p></div>',
+})
 
-new Vue({
-  el: "#dynamic-component-demo",
+var app = new Vue({
+  el: '#app',
   data: {
-    currentTab: "Home",
-    tabs: ["Home", "Posts", "Archive"]
-  },
-  computed: {
-    currentTabComponent: function() {
-      return "tab-" + this.currentTab.toLowerCase();
-    }
+    posts: [
+      {
+        id: 1,
+        title: 'teste1',
+      },
+      {
+        id: 2,
+        title: 'teste2',
+      },
+      {
+        id: 3,
+        title: 'teste3',
+      },
+      {
+        id: 4,
+        title: 'teste4',
+      },
+      {
+        id: 5,
+        title: 'teste5',
+      }
+    ]
   }
 });
