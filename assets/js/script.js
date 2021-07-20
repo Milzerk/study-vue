1d5713b30423da43600f953b4de6545a874cc0e8
Miller Kisley <milkisly@gmail.com>
2021-07-20 14:54:33 -0300

    slots nomeados

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,22 @@ Vue.component('menu-link', {
     `
 });
 
+Vue.component('base-layout', {
+  template: `
+      <div class="container">
+        <header>
+          <slot name="header"></slot>
+        </header>
+        <main>
+          <slot></slot>
+        </main>
+        <footer>
+          <slot name="footer"></slot>
+        </footer>
+      </div>
+    `
+});
+
 var vm = new Vue({
   el: "#app",
   data: {
